9857bf2021e3d96add26bb31c2fae05dc6301c7a
taras <taras.serba@gmail.com>
2018-09-11 15:34:14 +0300

    Service worker  tune

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Application from "./app/application/Application";
+import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+
 
 ReactDOM.render(<Application />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+if ('serviceWorker' in navigator) {
+    const registration = runtime.register();
+}
