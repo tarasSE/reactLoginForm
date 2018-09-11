98ff8f4ebecb72b1a4f0461deabfe5e9d242ddcd
taras <taras.serba@gmail.com>
2018-09-11 14:27:13 +0300

    Service worker  tune

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,17 +1,40 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = [
+const dataCacheName = 'LoginForm-v1';
+const cacheName = 'LoginForm-1';
+const filesToCache = [
     '/',
-    // '/styles/main.css',
-    // '/script/main.js'
+    '/index.html'
 ];
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
     );
 });
